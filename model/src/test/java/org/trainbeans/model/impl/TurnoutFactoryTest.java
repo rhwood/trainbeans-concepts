7ece75676e90004abde41fbc5a50e5c58b02df06
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-08-31 20:11:58 -0400

    test: add tests for TurnoutFactory

---
+++ model/src/test/java/org/trainbeans/model/impl/TurnoutFactoryTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 rhwood.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trainbeans.model.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
+import org.trainbeans.model.api.AbstractStatefulDelegate;
+import org.trainbeans.model.api.Turnout;
+import org.trainbeans.model.api.TurnoutDelegate;
+
+/**
+ *
+ * @author rhwood
+ */
+public class TurnoutFactoryTest {
+
+    private TurnoutFactory factory;
+    
+    @BeforeEach
+    public void setUp() {
+        factory = new TurnoutFactory();
+    }
+    
+    @AfterEach
+    public void tearDown() {
+        // nothing to do
+    }
+
+    @Test
+    public void testGetElementClass() {
+        assertThat(factory.getElementClass()).isEqualTo(Turnout.class);
+    }
+
+    @Test
+    public void testCreate() {
+        // empty Lookup
+        assertThat(factory.create("test", Lookup.EMPTY).getName()).isEqualTo("test");
+        assertThatThrownBy(() -> factory.create(null, Lookup.EMPTY)).isInstanceOf(IllegalArgumentException.class);
+        // null Lookup
+        assertThat(factory.create("test", null).getName()).isEqualTo("test");
+        assertThatThrownBy(() -> factory.create(null, null)).isInstanceOf(IllegalArgumentException.class);
+        // populated Lookup
+        TurnoutDelegate delegate = new TestTurnoutDelegate();
+        delegate.setName("delegate");
+        Lookup lookup = Lookups.fixed(delegate);
+        assertThat(factory.create(null, lookup).getName()).isEqualTo("delegate");
+        assertThat(factory.create("test", lookup).getName()).isEqualTo("test");
+    }
+    
+    private static class TestTurnoutDelegate extends AbstractStatefulDelegate<Turnout> implements TurnoutDelegate {
+
+        @Override
+        protected boolean isValidName(String name) {
+            return true;
+        }
+        
+    }
+}
