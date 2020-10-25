72f836352582368793f454ab69785d6f61b921ba
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-10-25 09:22:54 -0400

    test: additional testing

--- app/mr/src/test/java/org/trainbeans/app/mr/impl/MRAuxiliaryConfigurationTest.java
+++ app/mr/src/test/java/org/trainbeans/app/mr/impl/MRAuxiliaryConfigurationTest.java
@@ -17,12 +17,10 @@ package org.trainbeans.app.mr.impl;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -40,14 +38,12 @@ import org.w3c.dom.Element;
  */
 class MRAuxiliaryConfigurationTest {
 
-    private File workspace;
     private Document document;
     private ModelRailroadProject project;
     private AuxiliaryConfiguration config;
 
     @BeforeEach
     void setUp(@TempDir File testDir) throws ParserConfigurationException {
-        workspace = testDir;
         document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
         project = new ModelRailroadProject(FileUtil.toFileObject(testDir), Lookup.EMPTY);
         config = new MRAuxiliaryConfiguration(project, new ProjectState() {
@@ -68,9 +64,9 @@ class MRAuxiliaryConfigurationTest {
     void testGetConfigurationFragment(boolean arg) throws IOException {
         String PROJECT_XML = "trainbeans/project.xml";
         String PRIVATE_XML = "trainbeans/private.xml";
-        String id = Long.toString((new Date()).getTime());
         String elementName = "testElement";
         String namespace = "test";
+        // test "normal" conditions (read / write capable)
         assertThat(config.getConfigurationFragment(elementName, namespace, arg))
                 .isNull();
         Element e = document.createElementNS(namespace, elementName);
@@ -81,8 +77,15 @@ class MRAuxiliaryConfigurationTest {
         e = config.getConfigurationFragment(elementName, namespace, arg);
         assertThat(e).isNotNull();
         assertThat(e.getAttribute("foo")).isEqualTo("bar");
-        config.removeConfigurationFragment(elementName, namespace, arg);
-        assertThat(config.getConfigurationFragment(elementName, namespace, arg)).isNull();
+        // test unreadable conditions (not expected in normal operations)
+        File file = FileUtil.toFile(project.getProjectDirectory().getFileObject(arg ? PROJECT_XML : PRIVATE_XML));
+        file.setReadable(false);
+        e = config.getConfigurationFragment(elementName, namespace, arg);
+        assertThat(e).isNull();
+        file.setReadable(true);
+        e = config.getConfigurationFragment(elementName, namespace, arg);
+        assertThat(e).isNotNull();
+        assertThat(e.getAttribute("foo")).isEqualTo("bar");
     }
 
 
@@ -91,21 +94,35 @@ class MRAuxiliaryConfigurationTest {
     void testPutConfigurationFragment(boolean arg) throws IOException {
         String PROJECT_XML = "trainbeans/project.xml";
         String PRIVATE_XML = "trainbeans/private.xml";
-        String id = Long.toString((new Date()).getTime());
-        String elementName = "testElement";
+        String elementName1 = "testElement1";
+        String elementName2 = "testElement2";
         String namespace = "test";
-        assertThat(config.getConfigurationFragment(elementName, namespace, arg))
+        assertThat(config.getConfigurationFragment(elementName1, namespace, arg))
                 .isNull();
-        Element e = document.createElementNS(namespace, elementName);
+        Element e = document.createElementNS(namespace, elementName1);
         e.setAttribute("foo", "bar");
         config.putConfigurationFragment(e, arg);
         assertThat(project.getProjectDirectory().getFileObject(arg ? PROJECT_XML : PRIVATE_XML)).isNotNull();
         assertThat(project.getProjectDirectory().getFileObject(arg ? PRIVATE_XML : PROJECT_XML)).isNull();
-        e = config.getConfigurationFragment(elementName, namespace, arg);
+        e = config.getConfigurationFragment(elementName1, namespace, arg);
         assertThat(e).isNotNull();
         assertThat(e.getAttribute("foo")).isEqualTo("bar");
-        config.removeConfigurationFragment(elementName, namespace, arg);
-        assertThat(config.getConfigurationFragment(elementName, namespace, arg)).isNull();
+        e = document.createElementNS(namespace, elementName2);
+        e.setAttribute("foo", "bar");
+        config.putConfigurationFragment(e, arg);
+        e = config.getConfigurationFragment(elementName1, namespace, arg);
+        assertThat(e).isNotNull();
+        assertThat(e.getAttribute("foo")).isEqualTo("bar");
+        e = config.getConfigurationFragment(elementName2, namespace, arg);
+        assertThat(e).isNotNull();
+        assertThat(e.getAttribute("foo")).isEqualTo("bar");
+        e = document.createElementNS(namespace, elementName2);
+        e.setAttribute("bar", "foo");
+        config.putConfigurationFragment(e, arg);
+        e = config.getConfigurationFragment(elementName2, namespace, arg);
+        assertThat(e).isNotNull();
+        assertThat(e.getAttribute("bar")).isEqualTo("foo");
+        assertThat(e.getAttribute("foo")).isEmpty();
     }
 
     @ParameterizedTest
@@ -113,7 +130,6 @@ class MRAuxiliaryConfigurationTest {
     void testRemoveConfigurationFragment(boolean arg) throws IOException {
         String PROJECT_XML = "trainbeans/project.xml";
         String PRIVATE_XML = "trainbeans/private.xml";
-        String id = Long.toString((new Date()).getTime());
         String elementName = "testElement";
         String namespace = "test";
         assertThat(config.getConfigurationFragment(elementName, namespace, arg))
